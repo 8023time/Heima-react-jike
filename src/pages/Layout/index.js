40c665541907ef0b09ec8fde9c528743dd11bd8c
寻觅流光 <2162105974@qq.com>
2025-03-15 11:28:48 +0800

    updata 2025-3-15

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,10 @@
+const Layout = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to React</p>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
+  }
+  
+  export default Login;
